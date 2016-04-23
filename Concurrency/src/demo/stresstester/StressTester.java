775b978ccbd31fd9782363a9748da71b0a3720dc
John Näsman <nasmajoh@iki.fi>
2016-04-23 16:10:53 +0300

    concurrency esimerkki lisätty

---
+++ Concurrency/src/demo/stresstester/StressTester.java
@@ -0,0 +1,48 @@
+package demo.stresstester;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class StressTester {
+	public static void main(String[] args) throws InterruptedException {
+
+		// Server server = new Server();
+		final Collection<String> hetuList = Arrays.asList("160375-7476", "070175-685S", "300180-741P", "050150-263D",
+				"010852-131R", "071052-0675", "070252-7967", "170174-1379");
+
+		int nThreads = 20;
+
+		ExecutorService es = Executors.newFixedThreadPool(nThreads);
+		final CyclicBarrier barrier = new CyclicBarrier(nThreads);
+		for (int i = 0; i < nThreads; i++) {
+			es.submit(new Runnable() {
+				public void run() {
+					try {
+						barrier.await();
+						for (int i = 0; i < 100000; i++) {
+							for (String hetu : hetuList) {
+								try {
+									System.out.println("Do something with hetu " + hetu);
+								} catch (RuntimeException re) {
+									re.printStackTrace();
+									System.exit(-1);
+								}
+							}
+						}
+					} catch (InterruptedException | BrokenBarrierException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+
+		es.shutdown();
+		es.awaitTermination(20, TimeUnit.SECONDS);
+	}
+}
